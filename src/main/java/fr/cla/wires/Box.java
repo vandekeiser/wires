2455104726f91fd7ffef0610134e2947ed7455e5
Laurent CLAISSE <laurent_claisse@hotmail.fr>
2017-11-18 22:33:02 +0100

    ff

--- src/main/java/fr/cla/wires/Box.java
+++ src/main/java/fr/cla/wires/Box.java
@@ -33,13 +33,15 @@ public abstract class Box {
         );
     }
 
+    //----------Convenience shortcuts for Boxes that have 1 or 2 inputs, or N homogeneous inputs--------------VVVVVVVVVVVVVVVV
+    //----------(this follows the "Staged Builder" pattern)-------------
     protected <O, T> OnSignalChangedBuilder_ObservedWireCaptured<O, T> onSignalChanged(Wire<O> observedWire) {
         return new OnSignalChangedBuilder_ObservedWireCaptured<>(observedWire);
     }
 
-    //----------Convenience shortcuts for Boxes that have 1 or 2 inputs, or N homogeneous inputs--------------VVVVVVVVVVVVVVVV
-    //----------(this follows the "Staged Builder" pattern)-------------
 
+
+    
     /**
     * Captures the input wire to observe
     */
