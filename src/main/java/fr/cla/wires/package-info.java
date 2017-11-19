1b4f3e06c5010e56c78857452fdf28d2c4f16d4f
Laurent CLAISSE <laurent_claisse@hotmail.fr>
2017-11-19 18:24:14 +0100

    ff

--- src/main/java/fr/cla/wires/package-info.java
+++ src/main/java/fr/cla/wires/package-info.java
@@ -8,5 +8,9 @@
  *          -a convention of only accessing objects from 1 specific Thread, like the EDT in Swing
  *          -putting stuff into a ThreadLocal
  *      -"stack-confinement" (scoping local variables properly)
+ *
+ * TODO: where BinaryOperator<A> combiner / reduction is used,
+ *  indicate that that operator must be associative
+ *  & respect the other constraints from Collectors::reducing
  */
 package fr.cla.wires;
