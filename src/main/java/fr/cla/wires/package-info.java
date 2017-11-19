6cdbb98f48fb6500aa4959d658f8b6cb244949ec
Laurent CLAISSE <laurent_claisse@hotmail.fr>
2017-11-19 17:19:09 +0100

    documentation

--- src/main/java/fr/cla/wires/package-info.java
+++ src/main/java/fr/cla/wires/package-info.java
@@ -1,4 +1,12 @@
 /**
- * Take care that nothing is thread-safe!
+ * Take care that nothing in this package is thread-safe for now!
+ *
+ * Therefore, as explained in JCIP (Java concurrency in Practice),
+ *  thread-safety for users of this API can only be achieved
+ *  by using the following implementation patterns:
+ *      -"thread-confinement", by either:
+ *          -a convention of only accessing objects from 1 specific Thread, like the EDT in Swing
+ *          -putting stuff into a ThreadLocal
+ *      -"stack-confinement" (scoping local variables properly)
  */
 package fr.cla.wires;
