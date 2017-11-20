39e0e0a25579a1fbce3ae25875e70ce30257d6e4
Laurent CLAISSE <laurent_claisse@hotmail.fr>
2017-11-20 23:53:02 +0100

    Don't make OnSignalChanged package-private as this is the only alternative to the "Staged Builder" in Box, using Box::onSignalChanged

--- src/main/java/fr/cla/wires/OnSignalChanged.java
+++ src/main/java/fr/cla/wires/OnSignalChanged.java
@@ -4,8 +4,10 @@ import java.util.function.Consumer;
 
 //TODO: Could some composition methods be placed here?
 //@formatter:off
+//Don't make package-private as this is the only alternative
+// to the "Staged Builder" in Box, using Box::onSignalChanged
 @FunctionalInterface
-interface OnSignalChanged<V> extends Consumer<Signal<V>> {
+public interface OnSignalChanged<V> extends Consumer<Signal<V>> {
 
 }
 //@formatter:on
