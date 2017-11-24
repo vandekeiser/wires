4b751b602864957d43450507cb7816f6ebafa891
Laurent Claisse <laurent.claisse@zenika.com>
2017-11-24 23:21:12 +0100

    simplify

--- src/main/java/fr/cla/support/oo/Mutable.java
+++ src/main/java/fr/cla/support/oo/Mutable.java
@@ -1,7 +1,5 @@
 package fr.cla.support.oo;
 
-import java.util.Objects;
-
 import static java.util.Objects.requireNonNull;
 
 //@formatter:off
@@ -30,34 +28,6 @@ public class Mutable<T> {
     public final void set(T t) {
         this.current = requireNonNull(t);
     }
-
-    //----------Equality based solely on get()----------VVVVVVVVVVVVVVV
-    @Override public final boolean equals(Object obj) {
-        //An optimization, but also avoids StackOverflows on cyclic object graphs.
-        if(obj == this) return true;
-
-        if(! (obj instanceof Mutable)) return false;
-        Mutable<?> that = (Mutable<?>) obj;
-
-        return Objects.equals(
-            this.get(),
-            that.get()
-        );
-    }
-
-    @Override public final int hashCode() {
-        return Objects.hash(get());
-    }
-
-    @Override public String toString() {
-        return String.format(
-            "%s@%s%s",
-            getClass().getSimpleName(),
-            Integer.toHexString(System.identityHashCode(this)),
-            get()
-        );
-    }
-    //----------Equality based solely on get()----------^^^^^^^^^^^^^^^
 
 }
 //@formatter:on
