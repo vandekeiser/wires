93cc3d3a6fe71f927ce3e635251adcb96949600e
Laurent CLAISSE <laurent_claisse@hotmail.fr>
2017-11-27 17:22:21 +0100

    doc

--- src/main/java/fr/cla/wires/boxes/exampleusage/reentrant/package-info.java
+++ src/main/java/fr/cla/wires/boxes/exampleusage/reentrant/package-info.java
@@ -1,5 +1,5 @@
 /**
  * Proof that the model works with reentrant Wires,
- * that is out Wires which are plugged back into an input of the same Box.
+ * that is output Wires which are plugged back into an input of the same Box.
  */
 package fr.cla.wires.boxes.exampleusage.reentrant;
