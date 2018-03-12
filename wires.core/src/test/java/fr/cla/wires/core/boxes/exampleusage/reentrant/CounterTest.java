4986aa4b2aa83cfbf61d0915139d03ea96a3ffbc
Laurent CLAISSE <laurent_claisse@hotmail.fr>
2018-03-12 23:25:59 +0100

    same

--- wires.core/src/test/java/fr/cla/wires/core/boxes/exampleusage/reentrant/CounterTest.java
+++ wires.core/src/test/java/fr/cla/wires/core/boxes/exampleusage/reentrant/CounterTest.java
@@ -3,13 +3,11 @@ package fr.cla.wires.core.boxes.exampleusage.reentrant;
 
 import fr.cla.wires.core.Signal;
 import fr.cla.wires.core.Wire;
-import org.junit.Test;
 
 public class CounterTest {
 
     private Wire<Long> out;
 
-    @Test
     public void out_should_initially_be_0() {
         new Signal().equals(new Signal());
     }
