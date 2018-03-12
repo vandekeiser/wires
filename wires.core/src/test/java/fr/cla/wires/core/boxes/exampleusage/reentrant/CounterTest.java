8a6026760f3d389affe6005231d43b205adf053d
Laurent CLAISSE <laurent_claisse@hotmail.fr>
2018-03-12 23:07:00 +0100

    same

--- wires.core/src/test/java/fr/cla/wires/core/boxes/exampleusage/reentrant/CounterTest.java
+++ wires.core/src/test/java/fr/cla/wires/core/boxes/exampleusage/reentrant/CounterTest.java
@@ -1,10 +1,8 @@
 package fr.cla.wires.core.boxes.exampleusage.reentrant;
 
 
-import fr.cla.wires.core.Clock;
 import fr.cla.wires.core.Signal;
 import fr.cla.wires.core.Wire;
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,13 +15,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CounterTest {
 
     private Wire<Long> out;
-    private Clock clock;
-
-    @Before public void setup() {
-        out = Wire.make();
-        clock = Clock.createTime();
-        Counter.out(out).time(clock);
-    }
 
     @Test
     public void out_should_initially_be_0() {
