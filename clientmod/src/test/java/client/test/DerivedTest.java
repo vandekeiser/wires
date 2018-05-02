5c2824b8b00ad74e06cc52dbd08a83dca919b413
Laurent CLAISSE <laurent_claisse@hotmail.fr>
2018-05-02 22:04:42 +0200

    notes

--- clientmod/src/test/java/client/test/DerivedTest.java
+++ clientmod/src/test/java/client/test/DerivedTest.java
@@ -4,8 +4,12 @@ import client.Derived;
 
 public class DerivedTest {
 
-    public void should_foo() {
+    public void loading_an_src_main_class_from_the_api_module_should_not_crash() {
         new Derived().equals(null);
     }
-    
+
+    public static void main(String[] args) {
+        System.out.println("it works");
+    }
+
 }
