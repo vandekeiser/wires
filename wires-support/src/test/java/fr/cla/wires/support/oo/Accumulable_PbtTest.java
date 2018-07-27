15b0829eedb815dd0ddb4b92d1993c0e45ef2b8c
Laurent CLAISSE <laurent_claisse@hotmail.fr>
2018-07-28 01:35:00 +0200

    still repro

--- wires-support/src/test/java/fr/cla/wires/support/oo/Accumulable_PbtTest.java
+++ wires-support/src/test/java/fr/cla/wires/support/oo/Accumulable_PbtTest.java
@@ -8,6 +8,9 @@ import fr.cla.wires.support.oo.ddd.support.pbt.VoSingleton;
 
 public class Accumulable_PbtTest {
 
+//    [WARNING] Accumulable_PbtTest.
+// class fr.cla.wires.support.oo.ddd.support.pbt.VoSingleton
+// in module fr.cla.wires.support is not exported
     public void f (VoSingleton s) {}
 
 }
