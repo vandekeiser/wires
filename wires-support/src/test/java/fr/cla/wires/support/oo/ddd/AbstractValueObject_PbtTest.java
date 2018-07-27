4a5eac0b479ad59a5e3cbf6de999b0041e2ae95e
Laurent CLAISSE <laurent_claisse@hotmail.fr>
2018-07-28 01:21:44 +0200

    still repro

--- wires-support/src/test/java/fr/cla/wires/support/oo/ddd/AbstractValueObject_PbtTest.java
+++ wires-support/src/test/java/fr/cla/wires/support/oo/ddd/AbstractValueObject_PbtTest.java
@@ -6,12 +6,7 @@ import fr.cla.wires.support.oo.ddd.support.pbt.VoSingleton;
 //@formatter:off
 public class AbstractValueObject_PbtTest {
 
-    private static final int TRIALS = 100_000;
-
-    public void equals_should_be_reflexive(
-        VoSingleton s
-    ) {
-    }
+    public void f(VoSingleton s) {}
 
 }
 //@formatter:on
--- wires-support/src/test/java/fr/cla/wires/support/oo/ddd/support/pbt/VoSingleton.java
+++ wires-support/src/test/java/fr/cla/wires/support/oo/ddd/support/pbt/VoSingleton.java
@@ -1,17 +1,5 @@
 package fr.cla.wires.support.oo.ddd.support.pbt;
 
-import fr.cla.wires.support.oo.AbstractValueObject;
-
-import static java.util.Objects.requireNonNull;
-
-//@formatter:off
 public class VoSingleton {
 
-    public final AbstractValueObject<?> x;
-
-    public VoSingleton(AbstractValueObject<?> x) {
-        this.x = requireNonNull(x);
-    }
-
 }
-//@formatter:oN
