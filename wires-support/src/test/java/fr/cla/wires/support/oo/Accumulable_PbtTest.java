44cad33c87e13df9c08d32dcff92bb0e434b1599
Laurent CLAISSE <laurent_claisse@hotmail.fr>
2018-07-28 01:18:08 +0200

    still repro

--- wires-support/src/test/java/fr/cla/wires/support/oo/Accumulable_PbtTest.java
+++ wires-support/src/test/java/fr/cla/wires/support/oo/Accumulable_PbtTest.java
@@ -1,17 +1,12 @@
 package fr.cla.wires.support.oo;
 
-import com.pholser.junit.quickcheck.Property;
-import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
 import fr.cla.wires.support.oo.ddd.support.pbt.VoSingleton;
-import org.junit.runner.RunWith;
 
 
-@RunWith(JUnitQuickcheck.class)
 public class Accumulable_PbtTest {
 
     private static final int TRIALS = 100_000;
 
-    @Property(trials = TRIALS)
     public void x (
         VoSingleton initialAndNewValues
     ) {
--- wires-support/src/test/java/fr/cla/wires/support/oo/ddd/AbstractValueObject_PbtTest.java
+++ wires-support/src/test/java/fr/cla/wires/support/oo/ddd/AbstractValueObject_PbtTest.java
@@ -1,7 +1,6 @@
 package fr.cla.wires.support.oo.ddd;
 
 
-import com.pholser.junit.quickcheck.Property;
 import fr.cla.wires.support.oo.ddd.support.pbt.VoSingleton;
 
 //@formatter:off
@@ -9,7 +8,6 @@ public class AbstractValueObject_PbtTest {
 
     private static final int TRIALS = 100_000;
 
-    @Property(trials = TRIALS)
     public void equals_should_be_reflexive(
         VoSingleton s
     ) {
