7122fd4802426ef934b719c3c6d6d9df3c74bf7d
Laurent CLAISSE <laurent_claisse@hotmail.fr>
2018-07-28 01:11:22 +0200

    still repro

--- wires-support/src/test/java/fr/cla/wires/support/oo/ddd/AbstractValueObject_PbtTest.java
+++ wires-support/src/test/java/fr/cla/wires/support/oo/ddd/AbstractValueObject_PbtTest.java
@@ -3,12 +3,11 @@ package fr.cla.wires.support.oo.ddd;
 
 import com.pholser.junit.quickcheck.Property;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
-import fr.cla.wires.support.oo.ddd.support.pbt.*;
+import fr.cla.wires.support.oo.ddd.support.pbt.RandomVo;
+import fr.cla.wires.support.oo.ddd.support.pbt.VoSingleton;
 import org.junit.runner.RunWith;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 //@formatter:off
 @RunWith(JUnitQuickcheck.class)
@@ -23,61 +22,6 @@ public class AbstractValueObject_PbtTest {
         assertThat(
             s.x.equals(s.x)
         ).isTrue();
-    }
-
-    @Property(trials = TRIALS)
-    public void equals_should_be_symmetric(
-        @RandomVoPair VoPair p
-    ) {
-        assertThat(
-            p.x.equals(p.y)
-        ).isEqualTo(
-            p.y.equals(p.x)
-        );
-    }
-
-    @Property(trials = TRIALS)
-    public void equals_should_be_transitive(
-        @RandomVoTriplet VoTriplet t
-    ) {
-        if(t.x.equals(t.y) && t.y.equals(t.z)){
-            assertTrue(t.x.equals(t.z));
-        }
-    }
-
-    @Property(trials = TRIALS)
-    public void equals_implies_same_hashCode(
-        @RandomVoPair VoPair p
-    ) {
-        if(p.x.equals(p.y)){
-            assertEquals(p.x.hashCode(), p.y.hashCode());
-        }
-    }
-
-    /**
-     * Not part of the equals contract but part of the
-     */
-    @Property(trials = TRIALS)
-    public void equals_should_be_false_for_different_types(
-        @RandomVoPair VoPair p
-    ) {
-        if(!p.x.equals(p.y)) return;
-
-        assertThat(
-            p.x.getClass().equals(p.y.getClass())
-        )
-        .as(
-            "Expected only VOs of same time to be equal. Actual: %n" +
-            "    p.x: %s%n" +
-            "    p.y: %s%n" +
-            "    p.x.equals(p.y): %b%n" +
-            "    p.x.getClass(): %s%n" +
-            "    p.y.getClass(): %s%n" +
-            "    p.x.getClass().equals(p.y.getClass(): %b",
-            p.x, p.y, p.x.equals(p.y),
-            p.x.getClass(), p.y.getClass(), p.x.getClass().equals(p.y.getClass())
-        )
-        .isTrue();
     }
 
 }
