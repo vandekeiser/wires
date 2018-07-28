0ec1186893b203769da5cf501427ce016d479b95
Laurent CLAISSE <laurent_claisse@hotmail.fr>
2018-07-28 18:25:21 +0200

    cleanup SameTypePolicy

--- wires-support/src/main/java/fr/cla/wires/support/oo/AbstractValueObject.java
+++ wires-support/src/main/java/fr/cla/wires/support/oo/AbstractValueObject.java
@@ -3,6 +3,7 @@ package fr.cla.wires.support.oo;
 import java.util.List;
 import java.util.Objects;
 
+import static java.lang.String.format;
 import static java.util.Objects.requireNonNull;
 
 //@formatter:off
@@ -11,11 +12,17 @@ import static java.util.Objects.requireNonNull;
  * (it just doesn't have validation)
  */
 public abstract class AbstractValueObject<T extends AbstractValueObject<T>> {
-    
+
+    private final SameTypePolicy sameTypePolicy;
     private final Class<T> type;
 
     protected AbstractValueObject(Class<T> type) {
+        this(type, SameTypePolicy.SAME_CONCRETE_CLASS);
+    }
+
+    protected AbstractValueObject(Class<T> type, SameTypePolicy sameTypePolicy) {
         this.type = requireNonNull(type);
+        this.sameTypePolicy = requireNonNull(sameTypePolicy);
     }
 
     /**
@@ -26,11 +33,7 @@ public abstract class AbstractValueObject<T extends AbstractValueObject<T>> {
         //An optimization, but also avoids StackOverflows on cyclic object graphs.
         if(obj == this) return true;
 
-        SameTypePolicy x = null;
-        //KO, AbstractValueObject_PbtTest would fail as this doesn't prevent VO1A eq VO1B
-        //if(! type.isInstance(obj)) return false;
-        if(obj == null) return false;
-        if(! Objects.equals(this.getClass(), obj.getClass())) return false;
+        if(! sameTypePolicy.safeIsSameType(this, type, obj)) return false;
 
         T that = type.cast(obj);
         return Objects.equals(
@@ -56,8 +59,85 @@ public abstract class AbstractValueObject<T extends AbstractValueObject<T>> {
 
     protected abstract List<Object> equalityCriteria();
 
-    public boolean canEqual(AbstractValueObject<?> thisObj) {
-        return true;
+    /**
+     * @see SameTypePolicy.CAN_EQUAL
+     */
+    protected boolean canEqual(Object that) {
+        return that instanceof AbstractValueObject;
     }
+
+
+
+
+    public enum SameTypePolicy {
+        /**
+         *
+         */
+        SAME_CONCRETE_CLASS {
+            @Override <T> boolean isSameType(AbstractValueObject<?> thisObj, Class<T> thisObjType, AbstractValueObject<?> thatObj) {
+                return Objects.equals(thisObj.getClass(), thatObj.getClass());
+            }
+        },
+
+        /**
+         * KO if we want to keep AbstractValueObject_PbtTest::equals_should_be_false_for_different_types,
+         *  as this doesn't prevent VO1A eq VO1B
+         * OK otherwise.
+         *
+         * This has the advantage of working in the face of Hibernate proxies.
+         * It also allows a derived to compare equal to its parent while still respecting the Object::equals contract iff:
+         * -it adds no state compared to its parent class
+         *
+         * But is not always strict enough, as it doesn't allow distinguishing:
+         *  -derived classes that do add state and should not compare equal to their parent
+         *  -derived classes that do not add state and could compare equal to their parent
+         * That degree of fine-grained control requires using CAN_EQUAL,
+         *  and placing canEqual overrides in coordination with adding state or not.
+         *  (so this is still the responsibility of the derived class)
+         */
+        IS_INSTANCE{
+            @Override <T> boolean isSameType(AbstractValueObject<?> thisObj, Class<T> thisObjType, AbstractValueObject<?> thatObj) {
+                return thisObjType.isInstance(thatObj);
+            }
+        },
+
+        /**
+         * The most flexible pattern, initially from the Java bible "Effective Java".
+         * https://www.artima.com/lejava/articles/equality.html: nice article about the EJ "canEqual" pattern.
+         *
+         * This could allow to keep strict concrete class same-type policy when Hibernate modifies our class
+         *  but we want to ignore that from an equality point of view.
+         *
+         * TODO: test
+         * TODO: test with Hibernate-instrumented class where the concrete class is a load-type proxy
+         */
+        CAN_EQUAL{
+            @Override <T> boolean isSameType(AbstractValueObject<?> thisObj, Class<T> thisObjType, AbstractValueObject<?> thatObj) {
+                //Ensure equals stays symmetric
+                return
+                    thisObj.canEqual(thatObj)
+                    &&
+                    thatObj.canEqual(thisObj)
+                ;
+            }
+        },
+        ;
+
+        /**
+         * @throws NullPointerException iff thisObj or thisObjType is null (which is impossible from the equals method)
+         */
+        <T> boolean safeIsSameType(AbstractValueObject<?> thisObj, Class<T> thisObjType, Object thatObj) {
+            thisObj = requireNonNull(thisObj);
+            thisObjType = requireNonNull(thisObjType);
+
+            if(!(thatObj instanceof AbstractValueObject)) return false;
+            AbstractValueObject<?> thatVo = (AbstractValueObject<?>) thatObj;
+
+            return isSameType(thisObj, thisObjType, thatVo);
+        }
+
+        abstract <T> boolean isSameType(AbstractValueObject<?> thisObj, Class<T> thisObjType, AbstractValueObject<?> thatObj);
+    }
+
 }
 //@formatter:on
