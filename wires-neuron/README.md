6421182f5598f6d8d49d248214eb44482c7ecb58
Laurent CLAISSE <laurent_claisse@hotmail.fr>
2018-07-30 11:40:01 +0200

    leave only 2 modules

--- wires-neuron/README.md
+++
@@ -1,2 +0,0 @@
-Many useless classes here.
-The only interesting thing is the main class NeuronApp, which is used to test jlink packaging.
--- wires-neuron/src/main/java/module-info.java
+++
@@ -1,5 +0,0 @@
-//TODO cleanup all the useless stuff here
-module fr.cla.wires.neuron {
-    requires transitive fr.cla.wires.core;
-    exports fr.cla.wires.neuron;
-}
